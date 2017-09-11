bf3614b2b66681586522e723c796a1d10062b664
kobake <kobake@users.sourceforge.net>
2017-09-11 23:43:18 +0900

    Hello/World MVC

---
+++ AspNetCore.RouteAnalyzer.SampleWebProject/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.RouteAnalyzer.SampleWebProject.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult World()
+        {
+            return View();
+        }
+    }
+}
